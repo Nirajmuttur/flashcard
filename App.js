1e583a36ad7b86091ae89c11ee73a9552ebc1663
nirajmuttur <nirajmuttur@gmail.com>
2024-05-28 21:53:24 +0530

    update location, added appwrite package

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import { View, Text, SafeAreaView } from "react-native";
-import SplashScreen from "./Components/SplashScreen";
 
 //navigation
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Home from "./Components/Home";
-import LoginScreen from "./Components/LoginScreen";
-import MainTabNavigator from "./Components/MainTabNavigator";
-import SignUp from "./Components/SignUp";
+import LoginScreen from "./src/Components/LoginScreen";
+import SignUp from "./src/Components/SignUp";
+import MainTabNavigator from "./src/Components/MainTabNavigator"
+import SplashScreen from "./src/Components/SplashScreen";
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 function App() {
@@ -23,7 +22,7 @@ function App() {
         }}
       >
         <Stack.Screen name="SplashScreen" component={SplashScreen} />
-        <Stack.Screen name="LoginScreen" component={LoginScreen} options={{
+        <Stack.Screen name="LoginScreen" component={LoginScreenn} options={{
           headerShown: true,
           headerTitle: '',
           headerTintColor: "#00ADB5",
